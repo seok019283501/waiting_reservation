0040d8d3e2a4365bccfb69c977dce58810a0a537
minseok <alsthr0338@gmail.com>
2024-07-22 21:54:29 +0900

    feat: BE setting

---
+++ waiting_reservation_be/src/main/java/com/skshieldus/waiting_reservation_be/WaitingReservationBeApplication.java
@@ -0,0 +1,13 @@
+package com.skshieldus.waiting_reservation_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingReservationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitingReservationBeApplication.class, args);
+    }
+
+}
---
+++ waiting_reservation_be/src/test/java/com/skshieldus/waiting_reservation_be/WaitingReservationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skshieldus.waiting_reservation_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingReservationBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
